ad28f86a8b3bfa049947582bb05ab726eb4dcbe7
Mark Eastman <mark@eastmans.me.uk>
2015-12-03 16:08:03 +0000

    Initial version for data provisioning

---
+++ version_3/README.md
@@ -0,0 +1,2 @@
+# Tabella Version 3
+Extends the servlet system to provide some example screens for the application
---
+++ version_3/src/main/java/uk/me/eastmans/tabella/web/controller/HomeController.java
@@ -0,0 +1,32 @@
+package uk.me.eastmans.tabella.web.controller;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import uk.me.eastmans.tabella.service.BallotService;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+/**
+ * Created by meastman on 03/12/15.
+ */
+public class HomeController implements IThymeleafController
+{
+    @Inject
+    private BallotService ballotService;
+
+    public void process(
+            final HttpServletRequest request, final HttpServletResponse response,
+            final ServletContext servletContext, final TemplateEngine templateEngine)
+            throws Exception {
+
+        WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        ctx.setVariable("today", Calendar.getInstance());
+
+        templateEngine.process("home", ctx, response.getWriter());
+    }
+
+}
