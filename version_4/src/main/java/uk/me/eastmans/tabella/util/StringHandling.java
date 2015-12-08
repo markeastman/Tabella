90a15a106f374ab2a746f889e492b226fed077e6
Mark Eastman <mark@eastmans.me.uk>
2015-12-08 11:54:07 +0000

    Added a URLDecoder to get rid of http encoding

--- version_4/src/main/java/uk/me/eastmans/tabella/util/StringHandling.java
+++ version_4/src/main/java/uk/me/eastmans/tabella/util/StringHandling.java
@@ -3,6 +3,9 @@ package uk.me.eastmans.tabella.util;
 import javax.enterprise.inject.New;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,21 +25,21 @@ public class StringHandling
             BufferedReader reader = request.getReader();
             while ((line = reader.readLine()) != null)
                 jb.append(line);
-        } catch (Exception e) { /*report an error*/ }
-        // Values are in key=value&
-        String[] tokens = jb.toString().split("&");
-        for (String token : tokens)
-        {
-           // Now key = value
-            String[] mapping = token.split("=");
-            // Do we have the mapping already
-            if (!params.containsKey(mapping[0]))
-                params.put(mapping[0],new ArrayList<String>());
-            if (mapping.length > 1 && mapping[1] != null) {
-                List<String> values = params.get(mapping[0]);
-                values.add(mapping[1]);
+            // Values are in key=value&
+            String[] tokens = URLDecoder.decode(jb.toString(),"UTF8").split("&");
+            for (String token : tokens)
+            {
+               // Now key = value
+                String[] mapping = token.split("=");
+                // Do we have the mapping already
+                if (!params.containsKey(mapping[0]))
+                    params.put(mapping[0],new ArrayList<String>());
+                if (mapping.length > 1 && mapping[1] != null) {
+                    List<String> values = params.get(mapping[0]);
+                    values.add(mapping[1]);
+                }
             }
-        }
+        } catch (Exception e) { /*report an error*/ }
         return params;
     }
 }
