65d6ca4496bd803bc2a829d9bbe460368034aee1
Mark Eastman <mark@eastmans.me.uk>
2015-12-23 12:12:46 +0000

    Added current logged in name to home form

--- version_7/src/main/java/uk/me/eastmans/tabella/controllers/HomeController.java
+++ version_7/src/main/java/uk/me/eastmans/tabella/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package uk.me.eastmans.tabella.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.me.eastmans.tabella.services.BallotService;
+import uk.me.eastmans.tabella.services.CurrentUser;
 
 /**
  * Created by meastman on 22/12/15.
@@ -23,6 +25,8 @@ public class HomeController {
     @RequestMapping("/home")
     public String home(Model model)
     {
+//        Object currentUser = authentication.getPrincipal();
+//        model.addAttribute("currentUser", currentUser);
         model.addAttribute("ballots", ballotService.getAllOpenBallots());
         return "home";
     }
